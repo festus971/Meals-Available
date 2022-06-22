f48ee4110655c6689e17f3ded208ecf2f23c9586
+ <+>
2022-06-22 16:17:24 +0300

    deleting some unnecessary information at the moment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-// const btn=document.querySelector("button");
-// function random(number){
-//     return Math.floor(Math.random()*(number+1));
-
-// }
-// btn.addEventListener("click",()=>{
-//     const rndCol=`rgb(${random(255)},${random(255)},${random(0)})`;
-//     document.body.style.background=rndCol;
-// });
-// const button =document.getElementById("order-now")
-//const link="http:localhost:3000/categories"
 const menu=document.getElementById("category1")
 const image=document.getElementById("img")
-const category=document.getElementById("food-information")
+const category=document.getElementById("category")
 const thumb=document.getElementById("CategoryThumb")
 const description=document.getElementById("CategoryDescription")
 const button=document.getElementById("order-now")
@@ -19,9 +8,12 @@ const button=document.getElementById("order-now")
 
 function displayFood(foodObj){
     //category.innerText=foodObj.food-information
-    image.innerText=foodObj.image
+    // image.innerText=foodObj.img
+    category.innerText=foodObj.category
     thumb.innerText=foodObj.CategoryThumb
     description.innerText=foodObj.CategoryDescription
+
+    CategoryThumb.src=foodObj.CategoryThumb
     
 }
 fetch("http://localhost:3000/categories")
@@ -29,12 +21,12 @@ fetch("http://localhost:3000/categories")
 .then(jsonData=>{
     //console.log(jsonData)
 displayFood(jsonData[0])
-const foodList=document.getElementById("category1")
-jsonData.forEach(foodList=>{
+const foodList =document.getElementById("category1")
+jsonData.forEach(foodObj=>{
     const li=document.createElement("li")
     li.className="food-category"
-    li.innerText=foodList.category1
-    foodList.append(li)
+    li.innerText=foodObj.category1
+    foodObj.append(li)
     li.addEventListener("click",()=>{
         displayFood(foodObj)
     })
